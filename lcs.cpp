b16134534ce782ab2dfd5deae1a2573efd210eea
timtingwei <timtingwei@hotmail.com>
2018-01-06 08:41:23 +0800

    bug::unfinished

--- lcs.cpp
+++ lcs.cpp
@@ -19,26 +19,32 @@ int fib_dynamic(int n) {
   return g;
 }
 */
-/*
+
 char* lcs(char carr1[], int n, char carr2[], int m, char sub_s[], int n_r) {
-  // 返回数组carr_r为最大公共子串 carr1[0,n], carr2[0,m]
+  // carr1[0,n], carr2[0,m], 返回数组carr_r为最大公共子串 sub_s[]
+  // educationial
+  //    advantage
   if (n < 0 || m < 0) {
     n_r++;
     sub_s[n_r] = ' ';
-    return ' ';
+    std::cout << "-- -------n<0 || m<0-------- --\n"
+              << sub_s[0] << std::endl;
+    return sub_s;
   }
   if (carr1[n] == carr2[m]) {
     n_r++;
     sub_s[n_r] = carr1[n];
+    std::cout << "-- -------sub_s[n_r] = carr1[n]-------- --\n"
+              << sub_s[n_r] << std::endl;
     return lcs(carr1, n, carr2, m, sub_s, n_r);
   } else {
     // carr1[n] != carr2[m]
     lcs(carr1, n-1, carr2, m, sub_s, n_r);
     lcs(carr1, n, carr2, m-1, sub_s, n_r);
-    return;
+    return sub_s;
   }
 }
-*/
+
 void test_char(char carr[], int n) {
   std::cout << "*carr = " << *carr << '\n'
             << "sizeof(carr) " << sizeof(carr)/sizeof(carr[0]) << '\n'
@@ -63,13 +69,21 @@ char* fill_carr(char carr[], int n) {
 
 int main() {
   // ...
-
   char carr[] = {'a', 'b', 'c'};
   int n = 3;
   // test_char(carr, n);
   fill_carr(carr, n);
   std::cout << "-- ----print carr after fill_carr()----- -- " << std::endl;
   for (int i = 0; i < n; i++) {std::cout << *(carr+i) << std::endl;}
+
+  // -- --------lcs()---------- --
+  char carr1[] = {'a', 'd', 'v', 'a', 'n', 't', 'a', 'g', 'e'};
+  char carr2[] = {'e', 'd', 'u', 'c', 'a', 't', 'i', 'o', 'n', 'i', 'a', 'l'};
+  n = 8;
+  int m = 11;
+  char sub_s[] = {};
+  int n_r;
+  lcs(carr1, n, carr2, m, sub_s, n_r);
   return 0;
 }
 
