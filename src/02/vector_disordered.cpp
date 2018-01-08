c132c179f15fbf834cf81b235c1b81a773cea15a
timtingwei <timtingwei@hotmail.com>
2018-01-08 23:41:20 +0800

    overload operator[] in template

--- src/02/vector_disordered.cpp
+++ src/02/vector_disordered.cpp
@@ -2,13 +2,18 @@
     Copyright <2018> [Tim Hu]
     email: timtingwei@hotamail.com    
    ------------------------------
-   File:vector.cpp
-   Date:Sat Jan  6 08:41:27 CST 2018
+   File:vector_disordered.cpp
+   Date:Mon Jan  8 22:16:15 CST 2018
    -----------------------------
 */
+/*
+  定义无序向量, 没有顺序, 甚至不可能有顺序
+  - 寻秩访问
+ */
 
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 typedef int Rank;             // 秩
 #define DEAFAULT_CAPACITY 3   // 默认初始容量
@@ -19,8 +24,14 @@ template <typename T> class Vector {   // 向量模板类
  protected:
   /* ... 内部函数*/
  public:
-  // /* ... 构造函数
+  // 拷贝函数
   void copyFrom(T* const A, Rank lo, Rank hi);
+  // 扩容空间
+  void expand();
+
+  T& operator[](size_t n);
+
+  // /* ... 构造函数 */
   Vector(int c = DEAFAULT_CAPACITY)
   {_elem = new T[_capacity = c]; _size = 0;}     // 默认
   Vector(T* const A, Rank lo, Rank hi)          // 数组区间复制
@@ -29,10 +40,9 @@ template <typename T> class Vector {   // 向量模板类
   {copyFrom(V, lo, hi);}
   Vector(Vector<T> const& V)                     // 向量整体复制
   {copyFrom(V._elem, 0, V._size);}
-  //  */
+
   // /* ... 析构函数
   ~Vector() {delete [] _elem;}                   // 释放内部空间
-  // */
   /* ... 只读函数*/
   /* ... 可写函数*/
   /* ... 遍历函数*/
@@ -46,8 +56,46 @@ void Vector<T>::copyFrom(T* const A, Rank lo, Rank hi) {
     _elem[_size++] = A[lo++];
 }
 
+template <typename T>
+void Vector<T>::expand() {
+  if (_size < _capacity) return;    // 容量未到达不必扩容
+  // _capacity = max(_capacity, DEAFAULT_CAPACITY);    // 不小于最小容量
+  // 存储旧元素, 新数组容量扩大一倍
+  T* old_elem = _elem; _elem = new T[_capacity <<= 1];
+  for (int i = 0; i < _size; i++) {
+    _elem[i] = old_elem[i];   // 复制原数组到新数组的对应位置
+  }
+  delete [] old_elem;    // 释放原数组的对应空间，归还系统
+}
+
+/* 上述expand()中, new一个数组储存旧元素, 下面new一个数组, 作为扩容数组
+template <typename T>
+void Vector<T>::expand() {
+  if (_size < _capacity) return;    // 容量未到达不必扩容
+  _capacity = max(_capacity, DEAFAULT_CAPACITY);    // 不小于最小容量
+  // 存储旧元素, 新数组容量扩大
+  T* new_elem = new T[_capacity <<= 1];
+  for (int i = 0; i < _size; i++) {
+    new_elem[i] = _elem[i];   // 复制原数组到新数组的对应位置
+  }
+  // ERROR:不能直接赋值, 需要copyFrom(), 这里对_elem的更新不直接, 先新后旧的原则
+  _elem = new_elem;      // 当前数组元素更新为新数组元素
+  delete [] new_elem;    // 释放临时数组的对应空间，归还系统
+}
+*/
+
+// 寻秩访问
+// my test
+template <typename T>
+T& Vector<T>::operator[](std::size_t n) {    // 这个类 Vector<T>
+  assert(n < _size);
+  return _elem[n];
+}
+
+
 int main() {
   // ...
+  /*
   std::vector<int> vec;
   // vec.insert(0, 9, 2);
   // vec.put(1, 2);
@@ -66,6 +114,7 @@ int main() {
   // ---test array---
   // int i;
   // int arr[i = 5] = {};
+  */
   // -- ------test Vector class----- --
   int vi = 5;
   Rank vr = vi;
@@ -74,8 +123,10 @@ int main() {
 
   Rank lo = 0, hi = 5;
   int iarr[] = {1, 3, 5, 7, 2};
-  std::cout << "iarr = " << iarr << std::endl;
+  std::cout << "*iarr = " << *iarr << std::endl;
   Vector<int> v(iarr, lo, hi);
+
+  std::cout << "v[2] = " << v[2] << std::endl;
   return 0;
 }
 
